1404e0b701374c9dfbaad92edb2b5b4c8080e147
ayush <ayushmishra2005@gmail.com>
2016-06-13 21:40:23 +0530

    Added files

---
+++ src/main/scala/com/knoldus/Boot.scala
@@ -0,0 +1,26 @@
+package com.knoldus
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import com.knoldus.server.WebServer
+
+import scala.io.StdIn
+
+object Boot extends App with WebServer {
+
+  implicit val system = ActorSystem("my-system")
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext = system.dispatcher
+
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+    .onComplete(_ => system.terminate()) // and shutdown when done
+
+}
